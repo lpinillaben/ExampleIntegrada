2d9d1282099bc228f92bb5a430b4c11f98eddfe6
BOG-A209-E-015 <BOG-A209-E-015@RGH-LS6-15.UMD.LOCAL.EDU>
2019-02-09 08:52:19 -0500

    Se incluye libreria junit

---
+++ mavenproject1/src/main/java/helo.java
@@ -0,0 +1,19 @@
+import org.joda.time.LocalTime;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BOG-A209-E-015
+ */
+public class helo {
+
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+        System.out.println("Hola mundo");
+    }
+}
